b685a32890889176f3b471dea46f8d3d182409c7
Karthiga1 <45094294+Karthiga1@users.noreply.github.com>
2020-12-17 19:43:39 +0530

    Update build.py

--- build.py
+++ build.py
@@ -3,38 +3,25 @@ import os
 import shutil
 import zipfile
 
-source = 'C:/Users/kg52/OneDrive - DXC Production/Documents/Model/person.txt'
-destination = 'C:/Users/kg52/OneDrive - DXC Production/Documents/Model/builds'
 
 try:
-  with open('C:/Users/kg52/OneDrive - DXC Production/Documents/Model/Build.json') as f:
-    y = json.dumps(f.read())
-    data = json.loads(f.read())
-    parseFiles = []
+    with open('Build.json') as f:
+        data = json.loads(f.read())
 
-    for d in data:
-      print(d)
-      #name = d['name']
-      #content = d['content']
-      #print(d('Name'))
-      #print(d['content'])
+        for x in data:
+            StageName =  data[x]['Name']
+            StageContent = data[x]['Content']
 
-    #print(data['Stage1'])
-    #data1 = data['Stage1']
-    #print(data1['Name'])
-    #data2 = data1['Name']
+            if not os.path.exists('builds'):
+                os.makedirs('builds')
 
-    #if not os.path.exists('builds'):
-    #    os.makedirs('builds')
+            filepath = os.path.join("builds", StageName)   
 
-    #dest = shutil.move(source, destination)     
-    #shutil.make_archive('builds', 'zip', root_dir='.')
+            with open(filepath, 'w') as json_file:
+                json.dump(StageContent, json_file)  
 
+    shutil.make_archive('builds', 'zip', 'builds') 
 
-    
-    #with open('person.txt', 'w') as json_file:
-    #        json.dump(data, json_file) 
 finally:
     f.close()
-
-
+    json_file.close()
